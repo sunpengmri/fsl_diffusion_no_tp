53229612f3562bde240b0105b904b66a2a3e2190
sunpengmri <sunpengwipm@whu.edu.cn>
2022-12-06 14:51:58 +0800

    FreeSurfer recon

---
+++ bb_FS_pipeline/recon_all_parallel.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+
+WorkDir=/media/peng/data/02_MriDataSet/99_demo/15_ukbiobank/wang
+RawDir=$WorkDir/test
+export SUBJECTS_DIR=$WorkDir/FS
+# mkdir -p $SUBJECTS_DIR
+
+sublist="con002 con003"
+cd $RawDir
+mkdir temp
+cd temp
+for sub in ${sublist};  
+do  
+    cp ${RawDir}/${sub}/T1/T1_unbiased.nii.gz ./${sub}.nii.gz
+    gunzip ./${sub}.nii.gz
+done  
+cd $RawDir/temp
+ls *.nii | parallel --jobs 8 recon-all -s {.} -i {} -all -qcache
+rm -R $RawDir/temp
+
+
+
+
